e68598812ed30876ac679f89a6cd3b93e4ad96e8
jbock <jbock-java@gmx.de>
2023-01-12 22:09:28 +0100

    sort edges

--- compiler/src/main/java/io/jbock/simple/processor/ComponentGenerator.java
+++ compiler/src/main/java/io/jbock/simple/processor/ComponentGenerator.java
@@ -6,8 +6,11 @@ import io.jbock.simple.processor.binding.InjectBinding;
 import io.jbock.simple.processor.util.ComponentElement;
 import io.jbock.simple.processor.util.Edge;
 import io.jbock.simple.processor.util.InjectBindingRegistry;
+import io.jbock.simple.processor.util.ValidationFailure;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -21,26 +24,87 @@ public class ComponentGenerator {
         this.injectBindingRegistry = injectBindingRegistry;
     }
 
-    public TypeSpec generate(ComponentElement component) {
-        Set<Edge> dependencies = new LinkedHashSet<>();
-        for (DependencyRequest request : component.getRequests()) {
-            dependencies.addAll(injectBindingRegistry.getDependencies(request));
+    class Generator {
+        private final ComponentElement component;
+
+        Generator(ComponentElement component) {
+            this.component = component;
         }
-        return TypeSpec.classBuilder(component.generatedClass())
-                .addOriginatingElement(component.element())
-                .build();
+
+        public TypeSpec generate() {
+            Set<Edge> edges = new LinkedHashSet<>();
+            for (DependencyRequest request : component.getRequests()) {
+                edges.addAll(injectBindingRegistry.getDependencies(request));
+            }
+            List<InjectBinding> sorted = sortEdges(edges);
+            return TypeSpec.classBuilder(component.generatedClass())
+                    .addOriginatingElement(component.element())
+                    .build();
+        }
+
+        // https://en.wikipedia.org/wiki/Topological_sorting
+        private List<InjectBinding> sortEdges(Set<Edge> edges) {
+            List<InjectBinding> result = new ArrayList<>();
+            Deque<InjectBinding> s = new ArrayDeque<>(startNodes(edges));
+            Graph graph = createGraph(edges);
+            while (!s.isEmpty()) {
+                InjectBinding n = s.pop();
+                result.add(n);
+                for (Edge e : graph.edgesFrom(n)) {
+                    graph.removeEdge(e);
+                    InjectBinding m = e.destination();
+                    if (graph.edgesTo(m).isEmpty()) {
+                        s.push(m);
+                    }
+                }
+            }
+            if (!graph.edges.isEmpty()) {
+                throw new ValidationFailure("cycle detected", component.element());
+            }
+            return result;
+        }
+    }
+
+    public TypeSpec generate(ComponentElement component) {
+        return new Generator(component).generate();
     }
 
     private List<InjectBinding> startNodes(Set<Edge> edges) {
         List<InjectBinding> requests = edges.stream().map(Edge::dependency).toList();
         return requests.stream()
-                .filter(r -> edges.stream().noneMatch(edge -> edge.request().key().equals(r.key())))
+                .filter(r -> edges.stream().noneMatch(edge -> edge.destination().key().equals(r.key())))
                 .toList();
     }
 
-    private List<InjectBinding> toposort(Set<Edge> edges) {
-        List<InjectBinding> result = new ArrayList<>();
-        List<InjectBinding> startNodes = startNodes(edges);
-        return result;
+    private static class Graph {
+
+        final Set<Edge> edges;
+        final Set<InjectBinding> nodes;
+
+        Graph(Set<Edge> edges, Set<InjectBinding> nodes) {
+            this.edges = edges;
+            this.nodes = nodes;
+        }
+
+        List<Edge> edgesFrom(InjectBinding n) {
+            return edges.stream().filter(edge -> edge.source().equals(n)).toList();
+        }
+
+        List<Edge> edgesTo(InjectBinding m) {
+            return edges.stream().filter(edge -> edge.destination().equals(m)).toList();
+        }
+
+        void removeEdge(Edge edge) {
+            edges.remove(edge);
+        }
+    }
+
+    private Graph createGraph(Set<Edge> edges) {
+        Set<InjectBinding> nodes = new LinkedHashSet<>();
+        for (Edge edge : edges) {
+            nodes.add(edge.source());
+            nodes.add(edge.destination());
+        }
+        return new Graph(new LinkedHashSet<>(edges), nodes);
     }
 }
--- compiler/src/main/java/io/jbock/simple/processor/util/Edge.java
+++ compiler/src/main/java/io/jbock/simple/processor/util/Edge.java
@@ -3,4 +3,12 @@ package io.jbock.simple.processor.util;
 import io.jbock.simple.processor.binding.InjectBinding;
 
 public record Edge(InjectBinding request, InjectBinding dependency) {
+
+    public InjectBinding source() {
+        return dependency;
+    }
+
+    public InjectBinding destination() {
+        return request;
+    }
 }
