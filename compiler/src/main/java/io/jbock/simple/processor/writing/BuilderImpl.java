4056f5d826b0ab3338ea66edba41aa0820ad5e46
jbock <jbock-java@gmx.de>
2023-11-30 14:13:56 +0100

    ISSUES-4 builder build uses mock

--- compiler/src/main/java/io/jbock/simple/processor/writing/BuilderImpl.java
+++ compiler/src/main/java/io/jbock/simple/processor/writing/BuilderImpl.java
@@ -42,29 +42,99 @@ public class BuilderImpl {
     }
 
     TypeSpec generate(BuilderElement builder, MockBuilder mockBuilder) {
+        if (component.omitMockBuilder()) {
+            return generateNoMock(builder);
+        } else {
+            return generateMock(builder, mockBuilder);
+        }
+    }
+
+    private TypeSpec generateMock(BuilderElement builder, MockBuilder mockBuilder) {
         TypeMirror builderType = builder.element().asType();
         TypeSpec.Builder spec = TypeSpec.classBuilder(builder.generatedClass());
-        if (!component.omitMockBuilder()) {
-            FieldSpec field = FieldSpec.builder(mockBuilder.getClassName(), "mockBuilder", FINAL).build();
-            spec.addField(field);
-            ParameterSpec param = ParameterSpec.builder(mockBuilder.getClassName(), "mockBuilder").build();
-            spec.addMethod(MethodSpec.constructorBuilder()
-                    .addParameter(param)
-                    .addStatement("this.$N = $N", field, param)
-                    .build());
-        }
+        FieldSpec mockBuilderField = FieldSpec.builder(mockBuilder.getClassName(), "mockBuilder", FINAL).build();
+        spec.addField(mockBuilderField);
+        ParameterSpec mockBuilderParam = ParameterSpec.builder(mockBuilder.getClassName(), "mockBuilder").build();
+        spec.addMethod(MethodSpec.constructorBuilder()
+                .addParameter(mockBuilderParam)
+                .addStatement("this.$N = $N", mockBuilderField, mockBuilderParam)
+                .build());
         MethodSpec.Builder buildMethod = MethodSpec.methodBuilder(builder.buildMethod().getSimpleName().toString());
-        List<CodeBlock> constructorParameters = new ArrayList<>();
+        for (NamedBinding namedBinding : sorted.values()) {
+            Binding b = namedBinding.binding();
+            if (b instanceof ParameterBinding) {
+                continue;
+            }
+            Key key = b.key();
+            CodeBlock invocation = b.invocation(names);
+            ParameterSpec param = names.apply(key);
+            if (!key.typeName().isPrimitive()) {
+                buildMethod.addStatement("$1T $2N = this.$3N != null && this.$3N.$2N != null ? this.$3N.$2N : $4L",
+                        key.typeName(), param, mockBuilderField, invocation);
+            } else {
+                FieldSpec auxField = FieldSpec.builder(TypeName.BOOLEAN, namedBinding.auxName(), PRIVATE).build();
+                buildMethod.addStatement("$1T $2N = this.$3N != null && this.$3N.$4N ? this.$3N.$2N : $5L",
+                        key.typeName(), param, mockBuilderField, auxField, invocation);
+            }
+        }
+        spec.addFields(fields());
+        spec.addMethods(setterMethods(builder));
+        spec.addModifiers(PRIVATE, STATIC, FINAL);
+        spec.addSuperinterface(builderType);
+        buildMethod.addAnnotation(Override.class);
+        buildMethod.addModifiers(builder.buildMethod().getModifiers().stream()
+                .filter(m -> m == PUBLIC || m == PROTECTED).collect(Collectors.toList()));
+        buildMethod.returns(TypeName.get(component.element().asType()));
+        buildMethod.addStatement("return new $T($L)", component.generatedClass(), constructorParameters().stream()
+                .collect(CodeBlock.joining(", ")));
+        spec.addMethod(buildMethod.build());
+        return spec.build();
+    }
+
+    private TypeSpec generateNoMock(BuilderElement builder) {
+        TypeMirror builderType = builder.element().asType();
+        TypeSpec.Builder spec = TypeSpec.classBuilder(builder.generatedClass());
+        MethodSpec.Builder buildMethod = MethodSpec.methodBuilder(builder.buildMethod().getSimpleName().toString());
         for (NamedBinding namedBinding : sorted.values()) {
             Binding b = namedBinding.binding();
             Key key = b.key();
             CodeBlock invocation = b.invocation(names);
             ParameterSpec param = names.apply(key);
-            if (namedBinding.isComponentRequest()) {
-                constructorParameters.add(CodeBlock.of("$N", names.apply(key)));
+            if (!(b instanceof ParameterBinding)) {
+                buildMethod.addStatement("$T $N = $L", key.typeName(), param, invocation);
             }
+        }
+        spec.addFields(fields());
+        spec.addMethods(setterMethods(builder));
+        spec.addModifiers(PRIVATE, STATIC, FINAL);
+        spec.addSuperinterface(builderType);
+        buildMethod.addAnnotation(Override.class);
+        buildMethod.addModifiers(builder.buildMethod().getModifiers().stream()
+                .filter(m -> m == PUBLIC || m == PROTECTED).collect(Collectors.toList()));
+        buildMethod.returns(TypeName.get(component.element().asType()));
+        buildMethod.addStatement("return new $T($L)", component.generatedClass(), constructorParameters().stream()
+                .collect(CodeBlock.joining(", ")));
+        spec.addMethod(buildMethod.build());
+        return spec.build();
+    }
+
+    private List<FieldSpec> fields() {
+        List<FieldSpec> result = new ArrayList<>();
+        for (NamedBinding namedBinding : sorted.values()) {
+            Binding b = namedBinding.binding();
             if (b instanceof ParameterBinding) {
-                spec.addField(FieldSpec.builder(b.key().typeName(), names.apply(b.key()).name).build());
+                result.add(FieldSpec.builder(b.key().typeName(), names.apply(b.key()).name).build());
+            }
+        }
+        return result;
+    }
+
+    private List<MethodSpec> setterMethods(BuilderElement builder) {
+        TypeMirror builderType = builder.element().asType();
+        List<MethodSpec> result = new ArrayList<>();
+        for (NamedBinding namedBinding : sorted.values()) {
+            Binding b = namedBinding.binding();
+            if (b instanceof ParameterBinding) {
                 MethodSpec.Builder setterMethod = MethodSpec.methodBuilder(b.element().getSimpleName().toString());
                 setterMethod.addAnnotation(Override.class);
                 setterMethod.addParameter(names.apply(b.key()));
@@ -73,21 +143,22 @@ public class BuilderImpl {
                 setterMethod.returns(TypeName.get(builderType));
                 setterMethod.addModifiers(b.element().getModifiers().stream()
                         .filter(m -> m == PUBLIC || m == PROTECTED).collect(Collectors.toList()));
-                spec.addMethod(setterMethod.build());
-            } else {
-                buildMethod.addStatement("$T $N = $L", key.typeName(), param, invocation);
+                result.add(setterMethod.build());
             }
         }
-        spec.addModifiers(PRIVATE, STATIC, FINAL);
-        spec.addSuperinterface(builderType);
-        buildMethod.addAnnotation(Override.class);
-        buildMethod.addModifiers(builder.buildMethod().getModifiers().stream()
-                .filter(m -> m == PUBLIC || m == PROTECTED).collect(Collectors.toList()));
-        buildMethod.returns(TypeName.get(component.element().asType()));
-        buildMethod.addStatement("return new $T($L)", component.generatedClass(), constructorParameters.stream()
-                .collect(CodeBlock.joining(", ")));
-        spec.addMethod(buildMethod.build());
-        return spec.build();
+        return result;
+    }
+
+    private List<CodeBlock> constructorParameters() {
+        List<CodeBlock> result = new ArrayList<>();
+        for (NamedBinding namedBinding : sorted.values()) {
+            Binding b = namedBinding.binding();
+            Key key = b.key();
+            if (namedBinding.isComponentRequest()) {
+                result.add(CodeBlock.of("$N", names.apply(key)));
+            }
+        }
+        return result;
     }
 
     public static final class Factory {
