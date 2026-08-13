9ddc6b81283fdceddffc2ce18807e2df760487ea
Leonhard Bromoloron <lenobr@gmx.net>
2026-08-13 16:38:35 +0300

    update junit

--- simple-component/build.gradle.kts
+++ simple-component/build.gradle.kts
@@ -29,8 +29,9 @@ repositories {
 }
 
 dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation(platform("org.junit:junit-bom:6.1.2"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Jar> {
